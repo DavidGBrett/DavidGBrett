25d783bdf29d1f32f9e8f8a90dc306c39f5e7a10
David Brett <davidbrett2002@gmail.com>
2026-03-07 14:37:28 +0100

    Add function to store download stats in a json file by timestamp

--- scripts/calculate_downloads.py
+++ scripts/calculate_downloads.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+import json
 import os
 import requests
 
@@ -34,18 +36,51 @@ def get_sum_of_release_downloads(repo_name:str):
 
     return total_downloads
 
+def update_download_stats_file(repos_to_downloads:dict[str,int], total_profile_downloads:int):
+    stats_dir = "stats/"
+    os.makedirs(stats_dir, exist_ok=True)
+    
+    filepath = os.path.join(stats_dir, "repo_downloads.json")
+    current_timestamp = datetime.now().isoformat()
+    
+    # load stats data from stats file
+    data = {}
+    if os.path.exists(filepath):
+        try:
+            with open(filepath, 'r') as f:
+                data = json.load(f)
+
+        # if it doesn't exist or a bad file we will just write a new file later
+        except (json.JSONDecodeError, IOError):
+            data = {}
+    
+    # add new stats
+    data[current_timestamp] = {
+        "repos": repos_to_downloads,
+        "total": total_profile_downloads
+    }
+    
+    # write changes
+    with open(filepath, 'w') as f:
+        json.dump(data, f, indent=2, sort_keys=True)    
+
+
 
 
 if __name__ == "__main__":
     repo_names = get_public_repos_names()
 
     total_profile_downloads = 0
+    repos_to_downloads = {}
 
     for repo in repo_names:
         repo_downloads = get_sum_of_release_downloads(repo_name=repo)
 
         print(repo,repo_downloads)
+        repos_to_downloads[repo] = repo_downloads
 
         total_profile_downloads += repo_downloads
 
     print(total_profile_downloads)
+
+    update_download_stats_file(repos_to_downloads, total_profile_downloads)
