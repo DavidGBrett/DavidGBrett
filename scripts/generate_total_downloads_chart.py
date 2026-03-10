5875d450f49a9088693d55f9b1aec0e6065d364a
David Brett <davidbrett2002@gmail.com>
2026-03-10 14:44:14 +0100

    Remove outline and make title very light teal

--- scripts/generate_total_downloads_chart.py
+++ scripts/generate_total_downloads_chart.py
@@ -69,12 +69,11 @@ def make_chart(points: list[DataPoint], output_path: str = "charts/downloads.png
             alpha=0.9)
     
     ax.set_title("Downloads Across My Repositories", 
-                color='white', 
+                color="#9BD1CE", 
                 fontsize=14, 
                 fontweight='semibold', 
                 pad=20,
-                path_effects=[path_effects.Stroke(linewidth=2, foreground='black'),
-                         path_effects.Normal()])
+                )
     
     plt.suptitle(f'Latest Total: {totals[-1]}',
                 y = 0.8,
